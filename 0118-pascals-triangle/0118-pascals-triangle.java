62ca222fbc8d9e11c03604b89e0651fca639fa5b
despagoetry <129507427+despagoetry@users.noreply.github.com>
2026-07-01 20:36:47 +0530

    Time: 0 ms (100%), Space: 42.3 MB (99.96%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,19 @@
+import java.util.List;
+import java.util.ArrayList;
+class Solution {
+    
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i=0; i<numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            int num = 1;
+            for(int j=0; j<=i; j++) {
+                row.add(num);
+                num = num * (i-j)  / (j+1);
+            }
+            ans.add(row);
+        }
+        return ans;
+    }
+}
